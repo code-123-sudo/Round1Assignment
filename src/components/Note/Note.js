9a23a38efc88ee8789b2c28ea9ce0ba379fe6e9e
Shishir Jain <papa@Papas-MacBook-Pro.local>
2024-01-11 19:27:57 +0530

    set cursor at starting

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './Note.css';
 
 const Note = ({id , deleteNote, pinUnpinNote, pin}) => {
 
   const [content,setContent] = useState("")
+  const inputElement = useRef();
 
   const handleChange = (e) => {
     setContent(e.target.value);
   }
 
   const editNote = () => {
-
+    inputElement.current.setSelectionRange(0, 0);
+    inputElement.current.focus();
   }
 
   return (
@@ -19,7 +21,7 @@ const Note = ({id , deleteNote, pinUnpinNote, pin}) => {
       <div className="deleteButton" onClick={() => {deleteNote(id)}}><div>x</div></div>
       <div className="pinButton" onClick={() => {pinUnpinNote(id)}}><div>{ pin ? "UNPIN" : "PIN"}</div></div>
       <div className="inputBox">
-        <textarea type="text" value={content} onChange = {(e) => {handleChange(e)}} />
+        <textarea type="text" ref={inputElement} value={content} onChange = {(e) => {handleChange(e)}} />
       </div>
       <div className="editButton" onClick={() => {editNote(id)}}><div>EDIT</div></div>
     </div>
